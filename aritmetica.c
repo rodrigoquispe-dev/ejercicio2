d7fe282bff85f0f2e90692c97f9ea23d492fb9fa
Rodrigo Quispe <rodrigoquispe@gmail.com>
2026-03-16 16:37:07 -0400

    funciones aritmeticas

---
+++ aritmetica.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int suma(int a, int b){
+    return a + b;
+}
+
+int resta(int a, int b){
+    return a - b;
+}
+
+int multiplicacion(int a, int b){
+    return a * b;
+}
+
+int division(int a, int b){
+    return a / b;
+}
