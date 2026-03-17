dd06da88722ab0624ed65288b66dd05edc33066e
Melody Gutierrez Caero <202108274@est.umss.edu>
2026-03-16 23:10:59 -0400

    Funcion potencia Melody Gutierrez

--- aritmetica.c
+++ aritmetica.c
@@ -4,6 +4,17 @@
 int sumaRodrigoQuispe(int a, int b){
     return a + b;
 }
+// Funcion de Melody Alejandra Gutierrez Caero
+int potenciaMelodyGutierrez(int base, int exponente){
+
+    int resultado = 1;
+
+    for(int i = 0; i < exponente; i++){
+        resultado = resultado * base;
+    }
+
+    return resultado;
+}
 
 int main(){
 
@@ -12,6 +23,9 @@ int main(){
     r = sumaRodrigoQuispe(5,3);
 
     printf("Resultado: %d", r);
+    
+    r = potenciaMelodyGutierrez(2,3);
+    printf("Resultado potencia: %d\n", r);
 
     return 0;
 }
