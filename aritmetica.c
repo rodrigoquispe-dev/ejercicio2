eb8911252ebe1ea92d02cec648ac839cc50d987c
Jose Manuel Tellez Angulo <146342478+Manuu2102@users.noreply.github.com>
2026-03-16 23:36:26 -0400

    Se añadio funcion multiplicacion

--- aritmetica.c
+++ aritmetica.c
@@ -105,6 +105,26 @@ int main() {
     return 0;
 }
 
+//funcion de Manuel Tellez
+int multiplicacionDeValoresTellezManuel(){
+    int num1, num2;
+    
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &num1);
+    
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &num2);
+    
+    int resultado = num1 * num2;
+    return resultado;
+}
+int main(){
+    int res;
+    res = multiplicacionDeValoresTellezManuel();
+    printf("El resultado de la multiplicacion es: %d\n", res);
+    return 0;
+}
+
 
 
 
