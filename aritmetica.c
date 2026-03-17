5c45518b7cf715da1bd6a646ebe9107dc1f4bc57
José Alejandro Pardo Bautista <pardobautistaalejandro@gmail.com>
2026-03-16 23:22:28 -0400

    Función resta José Pardo

--- aritmetica.c
+++ aritmetica.c
@@ -1,34 +1,5 @@
 #include <stdio.h>
 
-// Funcion de Luis Huanca Mamani
-
-  GNU nano 8.7                                 aritmetica.c                                  Modified
-#include <stdio.h>
-
-// Funcion de Luis Huanca Mamani
-
-
-double valorAbsoluto(double numero){
-
-	if(numero < 0)
-		return -numero;
-	else
-		return numero;
-}
-//gonzalo raiz cuadrada
-#include <math.h>
-int main() {
-    double numero = 64.0;
-    // Elevamos a la 1/3
-    double resultado = pow(numero, 1.0/3.0);
-
-    printf("La raiz cubica de %.2f es %.2f\n", numero, resultado);
-    return 0;
-}
-//gonzalo raiz cuadrada
-
-
-
 // Funcion Resta de Jose Pardo
 int restaJosePardo(int a, int b){
     return a - b;
