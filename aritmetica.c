6543be2ce46f80744243f3095fa8aa23bb855d70
Carlos Mangudo <juniorbetancur715@gmail.com>
2026-03-16 23:15:18 -0400

    agrege el dividir

--- aritmetica.c
+++ aritmetica.c
@@ -27,6 +27,31 @@ int main() {
 }
 //gonzalo raiz cuadrada
 
+//Funcion de Dividir Carlos Mangudo
+
+#include <stdio.h>
+
+float dividirCarlosMangudo(int a, int b){
+    
+    if(b == 0){
+        printf("no se puede dividir entre 0\n");
+        return 0;
+    }
+
+    return (float)a / b;
+}
+
+int main(){
+    
+    int a = 10;
+    int b = 2;
+
+    float resultado = dividirCarlosMangudo(a, b);
+
+    printf("Resultado: %.2f\n", resultado);
+
+    return 0;
+}
 
 
 
