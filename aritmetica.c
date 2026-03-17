8455737bf1d60097e4c305345bb9430ab18e8496
José Alejandro Pardo Bautista <pardobautistaalejandro@gmail.com>
2026-03-16 20:17:30 -0400

    Función resta José Pardo

--- aritmetica.c
+++ aritmetica.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-// Funcion de Rodrigo Quispe Vargas
-int sumaRodrigoQuispe(int a, int b){
-    return a + b;
+// Funcion de José Alejandro Pardo Bautista
+int restaJosePardo(int a, int b){
+    return a - b;
 }
 
 int main(){
-
+    
     int r;
 
-    r = sumaRodrigoQuispe(5,3);
-
-    printf("Resultado: %d", r);
+    r = restaJosePardo(10, 4);
+    printf("Resultado Resta Jose Pardo: %d\n", r);
 
     return 0;
 }
