e2409ea25571308cb86289bafbd23eaf899a010e
José Alejandro Pardo Bautista <pardobautistaalejandro@gmail.com>
2026-03-16 23:14:14 -0400

    Función resta José Pardo

--- aritmetica.c
+++ aritmetica.c
@@ -29,6 +29,20 @@ int main() {
 
 
 
+// Funcion Resta de Jose Pardo
+int restaJosePardo(int a, int b){
+    return a - b;
+}
+
+int main(){
+    int r;
+
+    r = restaJosePardo(10, 4);
+    printf("Resultado Resta Jose Pardo: %d\n", r);
+
+    return 0;
+}
+// Funcion Resta de Jose Pardo
 
 
 
