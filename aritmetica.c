3b16678542ec8340dae7047eab27c7e52122d52e
rodrigoquispe-dev <quispe.vargas.rodrigo092002@gmail.com>
2026-03-16 23:43:14 -0400

    Update aritmetica.c

--- aritmetica.c
+++ aritmetica.c
@@ -125,7 +125,30 @@ int main(){
     return 0;
 }
 
+#include <stdio.h>
 
+// Funcion de Melody Alejandra Gutierrez Caero
+int potenciaMelodyGutierrez(int base, int exponente){
+
+    int resultado = 1;
+
+    for(int i = 0; i < exponente; i++){
+        resultado = resultado * base;
+    }
+
+    return resultado;
+}
+
+int main(){
+
+    int r;
+
+    r = potenciaMelodyGutierrez(2,3);
+
+    printf("Resultado potencia: %d\n", r);
+
+    return 0;
+}
 
 
 
