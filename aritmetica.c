5ba5ec7309271e42fad77ad2344dce9c58915cf0
rodrigoquispe-dev <quispe.vargas.rodrigo092002@gmail.com>
2026-03-16 23:30:43 -0400

    Update aritmetica.c

--- aritmetica.c
+++ aritmetica.c
@@ -69,7 +69,22 @@ int main(){
 }
 // Funcion Resta de Jose Pardo
 
+#include <stdio.h>
+// Funcion de Rodrigo Quispe Vargas
+int sumaRodrigoQuispe(int a, int b){
+    return a + b;
+}
 
+int main(){
+
+    int r;
+
+    r = sumaRodrigoQuispe(5,3);
+
+    printf("Resultado: %d", r);
+
+    return 0;
+}
 
 
 
